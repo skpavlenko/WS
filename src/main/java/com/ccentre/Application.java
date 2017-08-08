f87cf09c0e65cbb3e97ba2e24f552a9b06fd03a5
skpavlenko <skpavlenko@gmail.com>
2017-08-08 21:31:02 +0300

    Fix Tomcat problem

--- src/main/java/com/ccentre/Application.java
+++ src/main/java/com/ccentre/Application.java
@@ -4,20 +4,13 @@ package com.ccentre;
 import com.ccentre.service.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 import com.ccentre.entity.*;
 import com.ccentre.entity.enums.*;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
@@ -35,11 +28,4 @@ public class Application extends SpringBootServletInitializer {
             }
         };
     }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(applicationClass);
-    }
-
-    private static Class<Application> applicationClass = Application.class;
 }
