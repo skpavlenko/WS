ff047e0b7aa28661d3d026c043f615fccd5ba558
s.pavlenko <skpavlenko@gmail.com>
2017-08-09 17:30:50 +0300

    Try to fix Tomcat problem

--- src/main/java/com/ccentre/Application.java
+++ src/main/java/com/ccentre/Application.java
@@ -5,6 +5,7 @@ import com.ccentre.service.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
@@ -16,6 +17,13 @@ public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+
+    private static Class<Application> applicationClass = Application.class;
 
     @Bean
     public CommandLineRunner demo(final UserService userService) {
