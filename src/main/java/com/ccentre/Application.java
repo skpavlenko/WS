8a39837e74fa01e25ce172dfd5769c65f8a4f7f2
skpavlenko <skpavlenko@gmail.com>
2017-08-17 21:38:59 +0300

    Whitelabel issue

--- src/main/java/com/ccentre/Application.java
+++ src/main/java/com/ccentre/Application.java
@@ -4,24 +4,25 @@ package com.ccentre;
 import com.ccentre.service.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.ErrorPage;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import com.ccentre.entity.*;
 import com.ccentre.entity.enums.*;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
 @Configuration
-@ComponentScan
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
@@ -56,4 +57,19 @@ public class Application extends SpringBootServletInitializer {
         super.onStartup(servletContext);
     }
 
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+
+        return new EmbeddedServletContainerCustomizer() {
+            @Override
+            public void customize(ConfigurableEmbeddedServletContainer container) {
+
+                ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/error");
+                ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/error");
+                ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error");
+
+                container.addErrorPages(error401Page, error404Page, error500Page);
+            }
+        };
+    }
 }
